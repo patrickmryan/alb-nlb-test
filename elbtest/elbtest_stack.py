00e99cf5817b94cfd6a1ce5f0ffd5bcaefa21a20
Pat Ryan <pryan3@cisco.com>
2025-02-27 15:29:33 -0500

    working ALB w/ IAM server cert

--- elbtest/elbtest_stack.py
+++ elbtest/elbtest_stack.py
@@ -1,8 +1,9 @@
+import sys
 from aws_cdk import (
     # Duration,
     Stack,
     aws_ec2 as ec2,
-    aws_iam as iam,
+    # aws_iam as iam,
     aws_elasticloadbalancingv2 as elbv2,
     # aws_elasticloadbalancingv2_targets as elb_targets,
     aws_autoscaling as autoscaling,
@@ -11,19 +12,38 @@ from aws_cdk import (
 from constructs import Construct
 import boto3
 
+
 class ElbtestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # print(f"account={self.account}, region={self.region}", file=sys.stderr)
 
         ec2_client = boto3.client("ec2")
 
         vpc_name = self.node.try_get_context("VpcName")
 
+        results = ec2_client.describe_vpcs(
+            Filters=[
+                {
+                    "Name": "tag:Name",
+                    "Values": [vpc_name],
+                }
+            ]
+        )
+        if len(results["Vpcs"]) != 1:
+            raise ValueError(f"VPC {vpc_name} not found")
+
+        vpc_id = results["Vpcs"][0]["VpcId"]
         vpc = ec2.Vpc.from_lookup(
             self,
             "Vpc",
-            vpc_name=vpc_name,
+            # vpc_name=vpc_name,
+            # tags={"Name": vpc_name},
+            vpc_id=vpc_id,
         )
+        if not vpc:
+            raise ValueError(f"VPC {vpc_name} not found")
 
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Vpc
 
@@ -43,25 +63,43 @@ service httpd start
 
         intra_vpc = ec2.Peer.ipv4(vpc.vpc_cidr_block)
 
-        response = ec2_client.describe_subnets(
-            Filters=[
-                {
-                    "Name": "vpc-id",
-                    "Values": [vpc.vpc_id],
-                },
-                {
-                    "Name": "tag:aws-cdk:subnet-name",
-                    "Values": ["egress"],
-                },
+        # response = ec2_client.describe_subnets(
+        #     Filters=[
+        #         {
+        #             "Name": "vpc-id",
+        #             "Values": [vpc.vpc_id],
+        #         },
+        #         {
+        #             "Name": "tag:aws-cdk:subnet-name",
+        #             "Values": ["egress"],
+        #         },
+        #     ]
+        # )
+        # subnet_ids = [subnet["SubnetId"] for subnet in response["Subnets"]]
+
+        subnet_ids = self.get_named_subnets(vpc_id=vpc.vpc_id, subnet_name="egress")
+        egress_subnets = ec2.SubnetSelection(
+            subnets=[
+                ec2.Subnet.from_subnet_id(self, f"egress-subnet-{i}", subnet_id)
+                for i, subnet_id in enumerate(subnet_ids)
             ]
         )
-        subnet_ids = [subnet["SubnetId"] for subnet in response["Subnets"]]
+        for subnet in egress_subnets.subnets:
+            Annotations.of(subnet).acknowledge_warning(
+                "@aws-cdk/aws-ec2:noSubnetRouteTableId"
+            )
 
-        asg_subnets = ec2.SubnetSelection(
-            subnets=[ec2.Subnet.from_subnet_id(self, f"subnet-{i}", subnet_id) for i, subnet_id in enumerate(subnet_ids)]
+        subnet_ids = self.get_named_subnets(vpc_id=vpc.vpc_id, subnet_name="public")
+        public_subnets = ec2.SubnetSelection(
+            subnets=[
+                ec2.Subnet.from_subnet_id(self, f"public-subnet-{i}", subnet_id)
+                for i, subnet_id in enumerate(subnet_ids)
+            ]
         )
-        for subnet in asg_subnets.subnets:
-            Annotations.of(subnet).acknowledge_warning('@aws-cdk/aws-ec2:noSubnetRouteTableId')
+        for subnet in public_subnets.subnets:
+            Annotations.of(subnet).acknowledge_warning(
+                "@aws-cdk/aws-ec2:noSubnetRouteTableId"
+            )
 
         template_sg = ec2.SecurityGroup(self, "WebServerSG", vpc=vpc)
 
@@ -85,6 +123,7 @@ service httpd start
         )
 
         http_port = 80
+        https_port = 443
 
         template_sg.connections.allow_from(intra_vpc, ec2.Port.tcp(http_port))
         template_sg.connections.allow_from(intra_vpc, ec2.Port.tcp(22))
@@ -98,11 +137,12 @@ service httpd start
         lb_sg = ec2.SecurityGroup(self, "LBSG", vpc=vpc)
         lb_sg.connections.allow_from(intra_vpc, ec2.Port.all_traffic())
 
-
-        alb = elbv2.ApplicationLoadBalancer(self, "ALB",
+        alb = elbv2.ApplicationLoadBalancer(
+            self,
+            "ALB",
             vpc=vpc,
-            vpc_subnets=asg_subnets,
-            internet_facing=False
+            vpc_subnets=public_subnets,
+            internet_facing=True,  # False
         )
         alb.add_security_group(lb_sg)
 
@@ -118,7 +158,7 @@ service httpd start
             self,
             "ASG",
             vpc=vpc,
-            vpc_subnets=asg_subnets,
+            vpc_subnets=egress_subnets,
             launch_template=template,
             min_capacity=2,
             max_capacity=4,
@@ -130,34 +170,63 @@ service httpd start
         )
         asg.attach_to_application_target_group(application_target_group)
 
+        # https
+        # alb_listener = alb.add_listener(
+        #     "ALBlistener",
+        #     port=http_port,
+        #     default_action=elbv2.ListenerAction.forward([application_target_group]),
+        #     protocol=elbv2.ApplicationProtocol.HTTP
+        # )
+
         alb_listener = alb.add_listener(
             "ALBlistener",
-            port=http_port,
+            port=https_port,
             default_action=elbv2.ListenerAction.forward([application_target_group]),
-            protocol=elbv2.ApplicationProtocol.HTTP
+            protocol=elbv2.ApplicationProtocol.HTTPS,
+            certificates=[
+                elbv2.ListenerCertificate.from_arn(
+                    "arn:aws:iam::475158401257:server-certificate/cisco-lbtest"
+                )
+            ],
         )
-
 
         # create NLB, internal
         # VPC, isolated subnets
         # default s.g.
 
         # create the NLB that will sit in front of the ALB
-        nlb = elbv2.NetworkLoadBalancer(
-            self, "NLB",
-            vpc=vpc,
-            internet_facing=True,
-            vpc_subnets=asg_subnets
-        )
-        nlb.add_security_group(lb_sg)
-        nlb_listener = nlb.add_listener("NLBListener", port=http_port)
 
-        nlb_target_group = elbv2.NetworkTargetGroup(
-            self,
-            "NLBtargetgroup",
-            port=80,
-            vpc=vpc,
-            target_type=elbv2.TargetType.ALB,
+        # nlb = elbv2.NetworkLoadBalancer(
+        #     self, "NLB",
+        #     vpc=vpc,
+        #     internet_facing=True,
+        #     vpc_subnets=egress_subnets
+        # )
+        # nlb.add_security_group(lb_sg)
+        # nlb_listener = nlb.add_listener("NLBListener", port=http_port)
+
+        # nlb_target_group = elbv2.NetworkTargetGroup(
+        #     self,
+        #     "NLBtargetgroup",
+        #     port=80,
+        #     vpc=vpc,
+        #     target_type=elbv2.TargetType.ALB,
+        # )
+        # nlb_target_group.add_target(alb)
+        # nlb_listener.add_target_groups("AddTargetGroup", target_groups=[nlb_target_group])
+
+    def get_named_subnets(self, vpc_id=None, subnet_name=None):
+        ec2_client = boto3.client("ec2")
+        response = ec2_client.describe_subnets(
+            Filters=[
+                {
+                    "Name": "vpc-id",
+                    "Values": [vpc_id],
+                },
+                {
+                    "Name": "tag:aws-cdk:subnet-name",
+                    "Values": [subnet_name],
+                },
+            ]
         )
-        nlb_target_group.add_target(alb)
-        nlb_listener.add_target_groups("AddTargetGroup", target_groups=[nlb_target_group])
+        return [subnet["SubnetId"] for subnet in response["Subnets"]]
